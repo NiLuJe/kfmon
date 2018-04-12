b738d86a3192a3a674cf1f419bf65a25d56b2c55
NiLuJe <ninuje@gmail.com>
2018-04-12 16:11:30 +0200

    Patch on-animator to launch KFMon
    Making sure to avoid multiple instances, on the off-chance that could
    happen...

--- scripts/on-animator.sh
+++ scripts/on-animator.sh
@@ -3,9 +3,15 @@
 PRODUCT=`/bin/sh /bin/kobo_config.sh`;
 [ $PRODUCT != trilogy ] && PREFIX=$PRODUCT-
 
+# Launch KFMon if it isn't already running...
+if ! pkill -0 kfmon ; then
+        KFMON_BIN="/usr/local/kfmon/bin/kfmon"
+        [ -x "${KFMON_BIN}" ] && "${KFMON_BIN}" &
+fi
+
 i=0;
 while true; do
         i=$((((i + 1)) % 11));
         zcat /etc/images/$PREFIX\on-$i.raw.gz | /usr/local/Kobo/pickel showpic 1;
         usleep 250000;
-done 
+done
