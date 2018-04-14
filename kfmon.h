49e7e21e2fec1fead7eae51861065daabe941866
NiLuJe <ninuje@gmail.com>
2018-04-14 16:59:02 +0200

    Bump to v0.9.5

--- kfmon.h
+++ kfmon.h
@@ -48,7 +48,7 @@
 
 // Fallback version tag...
 #ifndef KFMON_VERSION
-#define KFMON_VERSION "v0.9"
+#define KFMON_VERSION "v0.9.5"
 #endif
 
 // Do an ifdef check to allow overriding those at compile-time...
