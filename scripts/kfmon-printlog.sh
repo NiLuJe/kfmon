b36809528eb7b52b343af541bbf14f7810aaf72e
NiLuJe <ninuje@gmail.com>
2018-06-16 02:22:40 +0200

    Tweak printlog script a bit...
    Avoid weird I/O related delays...

--- scripts/kfmon-printlog.sh
+++ scripts/kfmon-printlog.sh
@@ -9,10 +9,11 @@ FBINK_BIN="/usr/local/kfmon/bin/fbink"
 # Where's our log?
 KFMON_LOG="/usr/local/kfmon/kfmon.log"
 # How many lines do we want to print?
-# FIXME: May be a bit optimistic?
+# NOTE: Hard to pinpoint the right amount of stuff to print,
+#       since it depends both on the device and the actual content of the log...
 LOG_LINES="15"
 
 # And feed it to FBInk... (avoiding the first row because it's behind the bezel on my H2O ;p)
-${FBINK_BIN} -y 1 "$(tail -n ${LOG_LINES} "${KFMON_LOG}")"
+${FBINK_BIN} -q -y 1 "$(tail -n ${LOG_LINES} "${KFMON_LOG}")" >/dev/null 2>&1
 
 return 0
