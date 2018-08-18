e110d3073b064d67e85fa5f09f63a2cbfa1ae9d2
NiLuJe <ninuje@gmail.com>
2018-08-18 19:32:37 +0200

    That's probably better than an arbitrary magic percentage...

--- scripts/kfmon-printlog.sh
+++ scripts/kfmon-printlog.sh
@@ -9,13 +9,13 @@ FBINK_BIN="/usr/local/kfmon/bin/fbink"
 # Where's our log?
 KFMON_LOG="/usr/local/kfmon/kfmon.log"
 # How many lines do we want to print?
-# NOTE: We'll try to tailor that value to both the device and the content later on...
+# NOTE: Start high, we'll try to adjust it down to fit both the screen & the content later...
 LOG_LINES="25"
 
 # See how many lines we can actually print...
 eval $(${FBINK_BIN} -qe)
-# 62.5% is a slightly arbitrarily chosen value... We have to be conservative, because of linebreaks ;).
-MAXCHARS="$(awk -v MAXCOLS=${MAXCOLS} -v MAXROWS=${MAXROWS} 'BEGIN { print int((MAXCOLS * (MAXROWS - 1)) * 0.625) }')"
+# Try to account for linebreaks...
+MAXCHARS="$(awk -v LOG_LINES=${LOG_LINES} -v MAXCOLS=${MAXCOLS} -v MAXROWS=${MAXROWS} 'BEGIN { print int(MAXCOLS * (MAXROWS - LOG_LINES)) }')"
 
 # Check if we're logging to syslog instead...
 KFMON_CFG="/mnt/onboard/.adds/kfmon/config/kfmon.ini"
@@ -24,12 +24,16 @@ if grep use_syslog "${KFMON_CFG}" | grep -q -i -e 1 -e "on" -e "true" -e "yes" ;
 	# And see how many lines of that we can (roughly) print at most...
 	while [ "$(logread | grep -e KFMon -e FBInk | tail -n ${LOG_LINES} | wc -c)" -gt "${MAXCHARS}" ] ; do
 		let "LOG_LINES = ${LOG_LINES} - 1"
+		# Amount of lines changed, update that!
+		MAXCHARS="$(awk -v LOG_LINES=${LOG_LINES} -v MAXCOLS=${MAXCOLS} -v MAXROWS=${MAXROWS} 'BEGIN { print int(MAXCOLS * (MAXROWS - LOG_LINES)) }')"
 	done
 else
 	KFMON_USE_SYSLOG="false"
 	# And see how many lines of that we can (roughly) print at most...
 	while [ "$(tail -n ${LOG_LINES} "${KFMON_LOG}" | wc -c)" -gt "${MAXCHARS}" ] ; do
 		let "LOG_LINES = ${LOG_LINES} - 1"
+		# Amount of lines changed, update that!
+		MAXCHARS="$(awk -v LOG_LINES=${LOG_LINES} -v MAXCOLS=${MAXCOLS} -v MAXROWS=${MAXROWS} 'BEGIN { print int(MAXCOLS * (MAXROWS - LOG_LINES)) }')"
 	done
 fi
 
