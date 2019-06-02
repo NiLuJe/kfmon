a481735dbeefc2c2675b264cd8ccb84182bd86d3
NiLuJe <ninuje@gmail.com>
2019-06-02 23:41:18 +0200

    Well, it didn't horribly blow up, there's that :D.
    Now let's hope stuff is well behaved and sending a SIGTERM, and not a
    SIGKILL...

--- scripts/on-animator.sh
+++ scripts/on-animator.sh
@@ -19,10 +19,41 @@ else
 fi
 
 # Ditch the pickel progress bar for FBInk's, for shit'n giggles.
+# NOTE: This requires some extra shenanigans so that FBInk will properly get killed when on-animator gets the axe from Nickel...
+#       c.f., https://unix.stackexchange.com/a/444676
+prep_term()
+{
+	unset term_child_pid
+	unset term_kill_needed
+	trap 'handle_term' TERM INT
+}
+
+handle_term()
+{
+	if [ "${term_child_pid}" ]; then
+		kill -TERM "${term_child_pid}" 2>/dev/null
+	else
+		term_kill_needed="yes"
+	fi
+}
+
+wait_term()
+{
+	term_child_pid=$!
+	if [ "${term_kill_needed}" ]; then
+		kill -TERM "${term_child_pid}" 2>/dev/null
+	fi
+	wait ${term_child_pid}
+	trap - TERM INT
+	wait ${term_child_pid}
+}
+
 FBINK_BIN="/usr/local/kfmon/bin/fbink"
 
 # shellcheck disable=SC2046
 eval $(${FBINK_BIN} -e)
 
+prep_term
 # Make it fast, centered, and larger than usual.
-${FBINK_BIN} -q -M -W A2 -A -1 -S $(( FONTSIZE_MULT * 2 ))
+${FBINK_BIN} -q -M -W A2 -A -1 -S $(( FONTSIZE_MULT * 2 )) &
+wait_term
