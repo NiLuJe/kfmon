a119a32fbda747643f6b6489cf495e5e37c6452c
NiLuJe <ninuje@gmail.com>
2019-09-12 02:29:22 +0200

    Resync on-animator scripts w/ FW 4.16.13337

--- scripts/on-animator.sh
+++ scripts/on-animator.sh
@@ -56,7 +56,10 @@ else
 	i=0
 	while true ; do
 		i=$((((i + 1)) % 11))
-		zcat "/etc/images/${PREFIX}on-${i}.raw.gz" | /usr/local/Kobo/pickel showpic 1
-		usleep 250000
+		image="/etc/images/${PREFIX}on-${i}.raw.gz"
+		if [ -s "${image}" ] ; then
+			zcat "${image}" | /usr/local/Kobo/pickel showpic 1
+			usleep 250000
+		fi
 	done
 fi
--- scripts/uninstall/on-animator.sh
+++ scripts/uninstall/on-animator.sh
@@ -6,6 +6,9 @@ PRODUCT="$(/bin/sh /bin/kobo_config.sh)";
 i=0;
 while true; do
         i=$((((i + 1)) % 11));
-        zcat "/etc/images/${PREFIX}on-${i}.raw.gz" | /usr/local/Kobo/pickel showpic 1;
-        usleep 250000;
+        image="/etc/images/${PREFIX}on-${i}.raw.gz"
+        if [ -s "${image}" ] ; then
+            zcat "${image}" | /usr/local/Kobo/pickel showpic 1
+            usleep 250000
+        fi
 done
