45dd4671fd45b81d0f8af142817012538b6f0931
NiLuJe <ninuje@gmail.com>
2019-09-12 02:31:35 +0200

    Remove the rest of the trailing semi-colons

--- scripts/uninstall/on-animator.sh
+++ scripts/uninstall/on-animator.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-PRODUCT="$(/bin/sh /bin/kobo_config.sh)";
+PRODUCT="$(/bin/sh /bin/kobo_config.sh)"
 [ "${PRODUCT}" != "trilogy" ] && PREFIX="${PRODUCT}-"
 
-i=0;
-while true; do
-        i=$((((i + 1)) % 11));
+i=0
+while true ; do
+        i=$((((i + 1)) % 11))
         image="/etc/images/${PREFIX}on-${i}.raw.gz"
         if [ -s "${image}" ] ; then
             zcat "${image}" | /usr/local/Kobo/pickel showpic 1
