13f4fe1da8970207f82b1e6b6d4339a087b13b1c
NiLuJe <ninuje@gmail.com>
2020-04-26 19:30:54 +0200

    Simplify that

--- git/wrapper.c
+++ git/wrapper.c
@@ -6,26 +6,16 @@
  */
 #include "wrapper.h"
 
-/*
- * Limit size of IO chunks, because huge chunks only cause pain.  OS X
- * 64-bit is buggy, returning EINVAL if len >= INT_MAX; and even in
- * the absence of bugs, large chunks can result in bad latencies when
- * you decide to kill the process.
- *
- * We pick 8 MiB as our default, but if the platform defines SSIZE_MAX
- * that is smaller than that, clip it to SSIZE_MAX, as a call to
- * read(2) or write(2) larger than that is allowed to fail.  As the last
- * resort, we allow a port to pass via CFLAGS e.g. "-DMAX_IO_SIZE=value"
- * to override this, if the definition of SSIZE_MAX given by the platform
- * is broken.
- */
-#ifndef MAX_IO_SIZE
-#	define MAX_IO_SIZE_DEFAULT (8 * 1024 * 1024)
-#	if defined(SSIZE_MAX) && (SSIZE_MAX < MAX_IO_SIZE_DEFAULT)
-#		define MAX_IO_SIZE SSIZE_MAX
-#	else
-#		define MAX_IO_SIZE MAX_IO_SIZE_DEFAULT
-#	endif
+// Clamp IO chunks to the smaller of 8 MiB or SSIZE_MAX,
+// to deal with various implementation quirks on really old Linux,
+// macOS, or AIX/IRIX.
+// c.f., gnulib & busybox for similar stories.
+// Since we ourselves are 32 bit Linux-bound, 8 MiB suits us just fine.
+#define MAX_IO_SIZE_DEFAULT (8 * 1024 * 1024)
+#if defined(SSIZE_MAX) && (SSIZE_MAX < MAX_IO_SIZE_DEFAULT)
+#	define MAX_IO_SIZE SSIZE_MAX
+#else
+#	define MAX_IO_SIZE MAX_IO_SIZE_DEFAULT
 #endif
 
 static int
