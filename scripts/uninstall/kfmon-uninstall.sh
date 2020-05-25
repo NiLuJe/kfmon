960c557758789666ed5e510affd5e2eac3be9aca
NiLuJe <ninuje@gmail.com>
2020-05-26 01:52:22 +0200

    Make the uninstall script more thorough
    And take care of everything the OCP might install,
    as well as tripping NickelMenu's self-uninstall
    (https://github.com/geek1011/NickelMenu/issues/34)

--- scripts/uninstall/kfmon-uninstall.sh
+++ scripts/uninstall/kfmon-uninstall.sh
@@ -4,7 +4,7 @@
 rm -f "/etc/udev/rules.d/99-kfmon.rules"
 
 # NOTE: Make sure we're not running, first...
-pkill kfmon
+pkill -TERM kfmon
 
 # Uninstall: blow the full directroy tree away
 rm -rf "/usr/local/kfmon"
@@ -12,9 +12,20 @@ rm -rf "/usr/local/kfmon"
 # Also remove the kfmon-ipc symlink
 rm -f "/usr/bin/kfmon-ipc"
 
-# NOTE: We're leaving /mnt/onboard/kfmon.png & /mnt/onboard/koreader.png in,
-#       as well as basically whatever's in onboard, because that might contain user-created content,
-#       so, not our place to delete it.
+# NOTE: Delete our own icons & configs, but leave whatever else the user might have created alone.
+#       List based on what the full OCP deploys.
+for my_file in ".adds/kfmon/config/kfmon.ini" ".adds/kfmon/config/kfmon-log.ini" ".adds/kfmon/config/koreader.ini" ".adds/kfmon/config/plato.ini" ".adds/kfmon/bin/kfmon-printlog.sh" ".adds/kfmon/log/kfmon_dump.log" ".adds/nm/kfmon" ".adds/nm/koreader" ".adds/nm/plato" "icons/plato.png" "koreader.png" "kfmon.png" ; do
+	my_file="/mnt/onboard/${my_file}"
+	[ -f "${my_file}" ] && rm -f "${my_file}"
+done
+
+# Remove the directory if it's empty.
+# NOTE: This'll trip NickelMenu's self-uninstall ;).
+for my_dir in ".adds/kfmon/log" ".adds/kfmon/config" ".adds/kfmon/bin" ".adds/kfmon" ".adds/nm" "icons" ; do
+	my_dir="/mnt/onboard/${my_dir}"
+	# Because we can't have nice things, the stock busybox is too old to support -empty in find...
+	[ -d "${my_dir}" ] && [ -z "$(ls -A -- "${my_dir}")" ] && rm -rf "${my_dir}"
+done
 
 exit 0
 
