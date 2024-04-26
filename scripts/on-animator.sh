bdbbb5da9fd95b165c7215af1ffc02118e4eebbe
NiLuJe <ninuje@gmail.com>
2024-04-26 21:45:26 +0200

    Update the patched on-animator script for color panels

--- scripts/on-animator.sh
+++ scripts/on-animator.sh
@@ -107,13 +107,25 @@ if [ -f "${KFMON_BAR_FLAG}" ] ; then
 else
 	PRODUCT="$(/bin/sh /bin/kobo_config.sh)"
 	[ "${PRODUCT}" != "trilogy" ] && PREFIX="${PRODUCT}-"
+	COLOR="OFF"
+	if [ -e "/dev/mmcblk0p6" ] ; then
+		# We've only had color panels on MTK devices so far...
+		COLOR="$(ntx_hwconfig -S 1 -p /dev/mmcblk0p6 EPD_Flags CFA)"
+	fi
+
+	PARTIAL_UPDATE=1
+	if [ "${COLOR}" == "ON" ] ; then
+		# Send the first iteration as a FULL update on color panels
+		PARTIAL_UPDATE=0
+	fi
 
 	i=0
 	while true ; do
-		i=$((((i + 1)) % 11))
+		i=$(( (i + 1) % 11 ))
 		image="/etc/images/${PREFIX}on-${i}.raw.gz"
 		if [ -s "${image}" ] ; then
-			zcat "${image}" | /usr/local/Kobo/pickel showpic 1
+			zcat "${image}" | /usr/local/Kobo/pickel showpic ${PARTIAL_UPDATE}
+			PARTIAL_UPDATE=1
 			usleep 250000
 		fi
 	done
