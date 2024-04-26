55a5c0f14a63cf8a5d6d561d0772334fa824f978
NiLuJe <ninuje@gmail.com>
2024-04-26 21:40:05 +0200

    Start updating on-animator scripts for CFA panels support...

--- scripts/uninstall/on-animator.sh
+++ scripts/uninstall/on-animator.sh
@@ -2,13 +2,24 @@
 
 PRODUCT="$(/bin/sh /bin/kobo_config.sh)"
 [ "${PRODUCT}" != "trilogy" ] && PREFIX="${PRODUCT}-"
+COLOR="OFF"
+if [ -e "/dev/mmcblk0p6" ] ; then
+	# We've only had color panels on MTK devices so far...
+	COLOR="$(ntx_hwconfig -S 1 -p /dev/mmcblk0p6 EPD_Flags CFA)"
+fi
 
 i=0
+PARTIAL_UPDATE=0
 while true ; do
-        i=$((((i + 1)) % 11))
-        image="/etc/images/${PREFIX}on-${i}.raw.gz"
-        if [ -s "${image}" ] ; then
-            zcat "${image}" | /usr/local/Kobo/pickel showpic 1
-            usleep 250000
-        fi
+	i=$((((i + 1)) % 11))
+	image="/etc/images/${PREFIX}on-${i}.raw.gz"
+	if [ -s "${image}" ] ; then
+		if [ "${COLOR}" == "ON" ] ; then
+			zcat "${image}" | /usr/local/Kobo/pickel showpic ${PARTIAL_UPDATE}
+			PARTIAL_UPDATE=1
+		else
+			zcat "${image}" | /usr/local/Kobo/pickel showpic 1
+		fi
+		usleep 250000
+	fi
 done
