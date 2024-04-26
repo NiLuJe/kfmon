e4e114d60bac9ddd5d4d33edf821c09616453f8a
NiLuJe <ninuje@gmail.com>
2024-04-26 21:42:53 +0200

    Simplify stock on-animator logic

--- scripts/uninstall/on-animator.sh
+++ scripts/uninstall/on-animator.sh
@@ -8,18 +8,19 @@ if [ -e "/dev/mmcblk0p6" ] ; then
 	COLOR="$(ntx_hwconfig -S 1 -p /dev/mmcblk0p6 EPD_Flags CFA)"
 fi
 
+PARTIAL_UPDATE=1
+if [ "${COLOR}" == "ON" ] ; then
+	# Send the first iteration as a FULL update on color panels
+	PARTIAL_UPDATE=0
+fi
+
 i=0
-PARTIAL_UPDATE=0
 while true ; do
-	i=$((((i + 1)) % 11))
+	i=$(( (i + 1) % 11 ))
 	image="/etc/images/${PREFIX}on-${i}.raw.gz"
 	if [ -s "${image}" ] ; then
-		if [ "${COLOR}" == "ON" ] ; then
-			zcat "${image}" | /usr/local/Kobo/pickel showpic ${PARTIAL_UPDATE}
-			PARTIAL_UPDATE=1
-		else
-			zcat "${image}" | /usr/local/Kobo/pickel showpic 1
-		fi
+		zcat "${image}" | /usr/local/Kobo/pickel showpic ${PARTIAL_UPDATE}
+		PARTIAL_UPDATE=1
 		usleep 250000
 	fi
 done
