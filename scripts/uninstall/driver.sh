7fd018207e801586d062e3ec3f8b0dc5d888077c
NiLuJe <ninuje@gmail.com>
2024-08-25 14:47:06 +0200

    Minor comment & formatting tweaks to the OTA scripts
    (Also add a shebang to 'em)

--- scripts/uninstall/driver.sh
+++ scripts/uninstall/driver.sh
@@ -1,23 +1,25 @@
+#!/bin/sh
 # Kobo v5 update script, gets called by OTA update script.
 
 set -e
 # full path to update.tar
 ARCHIVE="$1"
-# called with stage1 first. If the script returns zero, reboot to recovery and called with stage2.
-# to avoid reboot to recovery, return non-zero (reboot will happen anyway)
+# called with stage1 first. If the script returns zero, the device reboots to recovery and this same script is called with stage2
+# to avoid a reboot to recovery, we always return non-zero (the reboot will happen anyway)
 STAGE="$2"
 
 mkdir -p /tmp/updater
 
 case $STAGE in
-    stage1)
-    # use run actual installer from script, so packaging multiple tools in one updater is easy
-    tar -C /tmp/updater -xf "${ARCHIVE}" "kfmon-uninstall.sh"
-    /tmp/updater/kfmon-uninstall.sh "${ARCHIVE}"
-    ;;
-    stage2)
-    ;;
+	stage1)
+		# pull the actual script we want to run from the archive, and run it from here, making packaging multiple tools in one updater easy
+		tar -C /tmp/updater -xf "${ARCHIVE}" "kfmon-uninstall.sh"
+		/tmp/updater/kfmon-uninstall.sh "${ARCHIVE}"
+	;;
+	stage2)
+	;;
 esac
+
 sync
 
 exit 1
